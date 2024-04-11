2a40b8a29dca99b1dee09b544443d670a7e6622e
Chancel KOTIN <chancelkotin24@gmail.com>
2024-04-11 11:24:51 +0100

    ajouter premier web

---
+++ Projet/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projet.Models;
+using System.Diagnostics;
+
+namespace Projet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        /*[HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // Chemin où le fichier sera sauvegardé
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", formFile.FileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Redirigez ou affichez un message de succès
+            return RedirectToAction("Index"); // Ou retournez un message de succès
+        }*/
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
