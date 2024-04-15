4be0460d5a40abfe07d5059a5b45eb99923d6a03
oceanelinxi <fuboya700@gmail.com>
2024-04-15 14:18:41 +0200

    add KNN et SVM

--- RFID_web/Controllers/HomeController.cs
+++ RFID_web/Controllers/HomeController.cs
@@ -6,6 +6,28 @@ using System.Web.Mvc;
 
 namespace RFID_web.Controllers
 {
+    /*[HttpPost]
+      public async Task<IActionResult> Upload(List<IFormFile> files)
+      {
+          long size = files.Sum(f => f.Length);
+
+          foreach (var formFile in files)
+          {
+              if (formFile.Length > 0)
+              {
+                  // Chemin où le fichier sera sauvegardé
+                  var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", formFile.FileName);
+
+                  using (var stream = new FileStream(filePath, FileMode.Create))
+                  {
+                      await formFile.CopyToAsync(stream);
+                  }
+              }
+          }
+
+          // Redirigez ou affichez un message de succès
+          return RedirectToAction("Index"); // Ou retournez un message de succès
+      }*/
     public class HomeController : Controller
     {
         public ActionResult Index()
