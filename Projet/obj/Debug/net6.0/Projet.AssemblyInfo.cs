0fca8c0a51ed64ada9778a9d2e4cfa71bffc0ab4
superlordandus <achraf.souley@groupe-esigelec.org>
2024-04-16 17:00:39 +0100

    "essai" Please enter the commit message for your changes. Lines starting

--- Projet/obj/Debug/net6.0/Projet.AssemblyInfo.cs
+++ Projet/obj/Debug/net6.0/Projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a40b8a29dca99b1dee09b544443d670a7e6622e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b50be6ebe1aebba5021cfaca026025a349d1aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
