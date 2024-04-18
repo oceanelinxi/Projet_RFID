203af363593a3d128c8543c6afe93eba389b3f38
Randy TCHUISSEU <randytchuiss@gmail.com>
2024-04-18 11:43:51 +0200

    Vue méthode analytique et méthodes du controleur Home

--- RFID_web/Controllers/HomeController.cs
+++ RFID_web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace RFID_web.Controllers
 {
@@ -13,6 +17,27 @@ namespace RFID_web.Controllers
             return View();
         }
 
+            
+        public ActionResult Analytic()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Methode_analytique()
+        {
+            using (var client = new HttpClient())
+            {
+                var requestData = new {};
+
+                var content = new StringContent(JsonConvert.SerializeObject(requestData), System.Text.Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://localhost:5000/analytique", content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                ViewBag.Result = result;
+            }
+
+            return View("ResultatAnalytique");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -40,7 +65,7 @@ namespace RFID_web.Controllers
 
             return View();
         }
+       
 
-    
     }
 }
