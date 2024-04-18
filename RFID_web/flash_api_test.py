d676099f15c79667ec61c0aeaeb55feeaf89c5f7
Randy TCHUISSEU <randytchuiss@gmail.com>
2024-04-18 18:13:28 +0200

    SVM

--- RFID_web/flash_api_test.py
+++ RFID_web/flash_api_test.py
@@ -14,3 +14,17 @@ def analytique():
     return jsonify({'accuracy': accuracy})
 
 app.run(host='0.0.0.0', port=5000)
+
+@app.route('/SVM', methods=['POST'])
+def svm():
+    # Get the input parameters from the request
+    input_params = request.get_json()
+    print(input_params)
+    print((input_params['Gamma'], input_params['C'], input_params['Kernel']))
+    # Call the predict() function to make a prediction with SVM
+    accuracy = train_and_evaluate_svm(input_params['Gamma'], input_params['C'], input_params['Kernel'])
+    
+    # Return the prediction as JSON
+    return jsonify({'accuracy': accuracy})
+
+app.run(host='0.0.0.0', port=5000)
