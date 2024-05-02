42f7c4629750385b2f6fe6c072d8324be1e5f45a
oceanelinxi <fuboya700@gmail.com>
2024-05-03 00:08:26 +0200

    ML new

--- MLnew/MLnew/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MLnew.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
