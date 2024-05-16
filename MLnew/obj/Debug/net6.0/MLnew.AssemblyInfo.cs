6c5a500bfbede58268b583b794ded6d4658c29e8
oceanelinxi <fuboya700@gmail.com>
2024-05-17 00:12:18 +0200

    add color index page

--- MLnew/obj/Debug/net6.0/MLnew.AssemblyInfo.cs
+++ MLnew/obj/Debug/net6.0/MLnew.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLnew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ff64814870a05d59079257c1001d21d7d220cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432ee3244ffab7dd2bb8731ca16ea7c3bd99ea0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLnew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLnew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
