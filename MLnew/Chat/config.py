0e4805fea189b3ae2839ef6262e2dc7f941fe2a8
oceanelinxi <95941865+oceanelinxi@users.noreply.github.com>
2024-05-27 17:56:39 +0200

    chatbot
    chatbot

---
+++ MLnew/Chat/config.py
@@ -0,0 +1,3 @@
+OPENAI_API_KEY=""# Entrez votre clé API OpenAI, ou entrez "export OPENAI_API_KEY='sk-...'" dans le terminal pour la définir comme variable d'environnement
+PINECONE_API_KEY="" # Entrez votre clé API Pinecone, consultez la page "API Keys" sur app.pinecone.io
+PINECONE_API_ENV="us-east-1" # Entrez votre environnement API Pinecone
