1e03acea40905aaab10bb35b92ce8de225008993
oceanelinxi <95941865+oceanelinxi@users.noreply.github.com>
2024-05-27 18:04:38 +0200

    add openai api config.py

--- MLnew/Chat/config.py
+++ MLnew/Chat/config.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY=""# Entrez votre clé API OpenAI, ou entrez "export OPENAI_API_KEY='sk-...'" dans le terminal pour la définir comme variable d'environnement
+OPENAI_API_KEY="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"# Entrez votre clé API OpenAI, ou entrez "export OPENAI_API_KEY='sk-...'" dans le terminal pour la définir comme variable d'environnement
 PINECONE_API_KEY="4425a25f-74e9-4a80-9ad2-9e285d906068" # Entrez votre clé API Pinecone, consultez la page "API Keys" sur app.pinecone.io
 PINECONE_API_ENV="us-east-1" # Entrez votre environnement API Pinecone
