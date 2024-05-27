a793d5a3d44b486e8da336ce5fd925e0a5dbc6ac
oceanelinxi <95941865+oceanelinxi@users.noreply.github.com>
2024-05-27 17:58:21 +0200

    add chatbot dans HomeController.cs

--- MLnew/Controllers/HomeController.cs
+++ MLnew/Controllers/HomeController.cs
@@ -1495,5 +1495,10 @@ namespace MLnew.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+         public ActionResult ChatBot()
+         {
+             return View();
+         }
     }
 }
